7c619da1f3de22fd6dc5cf1e454b2f73642c3274
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2021-01-11 08:05:04 -0500

    [find_max] Add function

--- find_max.py
+++ find_max.py
@@ -5,7 +5,19 @@
 
 def find_max(l):
     # Write code here
-    pass
+    if type(l) != list:
+        raise TypeError('Must be entered as list')
+    if len(l) < 1:
+        raise ValueError('List must contain at least one index')
+    if len(l) == 1:
+        return l[0]
+    else:
+        max_value = find_max(l[1:])
+        if max_value > l[0]:
+            return max_value
+        else:
+            return l[0]
 
-# print(find_max([1, 4, 45, 6, -50, 10, 2]))
+print(find_max([1, 4, 45, 6, -50, 10, 2]))
+print(find_max([1, 4, 45, 6, 70, 10, 82]))
 # => 45
