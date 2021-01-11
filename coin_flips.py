12637232aa36d9a6f675ec8771bcefe8d7188b48
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2021-01-11 07:43:03 -0500

    [coin_flips] Add function

--- coin_flips.py
+++ coin_flips.py
@@ -8,7 +8,23 @@
 
 def coin_flips(n):
     # Write code here
-    pass
+    if type(n) != int:
+        raise TypeError('Value must be an integer')
+    if n < 1:
+        raise ValueError('Must be greater than zero')
 
-# print(coinFlips(2)) 
+    # Base case
+    if n == 1:
+        results = ['H', 'T']
+        return results
+    else:
+        possible_results = ['H', 'T']
+        results = []
+        for i in possible_results:
+            for result in coin_flips(n -1):
+                results.append(i + result)
+        return results
+
+
+print(coin_flips(2)) 
 # => ["HH", "HT", "TH", "TT"]
