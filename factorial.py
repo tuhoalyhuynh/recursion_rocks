ded27009f91f3d467fdc2a494659931f0e70e24f
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2021-01-11 07:46:55 -0500

    [factorial] Add function

--- factorial.py
+++ factorial.py
@@ -1,11 +1,20 @@
+from functools import lru_cache
 # You will have to figure out what parameters to include
 # 🚨 All functions must use recursion 🚨
 
 # This function returns the factorial of a given number.
-
+@lru_cache(maxsize=100)
 def factorial(n):
     # Write code here
-    pass
+    if type(n) != int:
+        raise TypeError('Value must be a positive number')
+    if n < 1:
+        raise ValueError('Number needs to be positive')
 
-# print(factorial(5))
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(5))
 # => 120
